a615f43770e68f8324e94277437e1e9661220b55
subhojit80014-lang <subhojit80014@gmail.com>
2025-07-16 14:53:43 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -68,10 +68,10 @@ LINK_WHITELIST: "youtube.com,github.com",
           DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 
-          OWNER_NUMBER: process.env.OWNER_NUMBER || "8001416344",
+          OWNER_NUMBER: process.env.OWNER_NUMBER || "8695169973",
 // add your bot owner number
 
-OWNER_NAME: process.env.OWNER_NAME || "rdx",
+OWNER_NAME: process.env.OWNER_NAME || "Doramone",
 // add bot owner name
 
               DESCRIPTION: process.env.DESCRIPTION || "*© Powered By Lucky Tech Hub*",
